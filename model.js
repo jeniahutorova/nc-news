0bdaafd42b235c300011e966bd26bd2350593523
janehutorova <jeniavizir@gmail.com>
2024-02-19 11:33:52 +0000

    Test for errors complete

--- model.js
+++ model.js
@@ -2,7 +2,6 @@ const db = require('./db/connection')
 
 exports.selectTopics = () => {
     return db.query('SELECT * FROM topics').then((topics) => {
-        console.log(topics)
         return { topics: topics.rows };
       });
 }
