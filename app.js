27da4a56ffb3bdc1d8ec1187249ee48ce9518371
janehutorova <jeniavizir@gmail.com>
2024-02-19 14:18:20 +0000

    Tests for error handling at kata 4 complete

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express')
-const {getTopics} = require("./controller")
+const {getTopics, getArticleById} = require("./controller")
 const app = express();
 
 app.get("/api/topics", getTopics)
+app.get("/api/articles/:article_id", getArticleById)
 
+app.use((err, request, response, next)=> {
+    if (err.status && err.msg) {
+      response.status(err.status).send({ msg: err.msg })
+    }
+    next(err)
+  })
+app.use((err, request, response, next) => {
+    response.status(500).send({msg :'Internal Server Error'})
+    next(err)
+})
 app.all('/*', (request, response, next) => {
     response.status(404).send({msg: "Endpoint not found"})
     next(err)
   })
+
 module.exports = app
--- nc_news.test.js
+++ nc_news.test.js
@@ -3,13 +3,14 @@ const seed = require("./db/seeds/seed");
 const data = require("./db/data/test-data");
 const request = require("supertest");
 const app = require("./app");
-
+const endpoints = require('./endpoints.json')
+const fs = require('fs')
 
 beforeAll(() => seed(data));
 afterAll(() => db.end());
 
 describe('GET /api/topics', () => {
-    it('should be available on endpoint /api/topics', () => {
+    it('should an array of topic objects, each of which should have the following properties', () => {
         return request(app)
         .get('/api/topics')
         .expect(200)
@@ -30,5 +31,37 @@ describe('GET /api/topics', () => {
                 expect(response.status).toBe(404)
                 expect(response.body.msg).toBe("Endpoint not found")
         })
+    })
+    it('an article object, which should have the following properties', () => {
+        return request(app)
+        .get('/api/articles/1')
+        .expect(200)
+        .then((response) => {
+            const {article} = response.body;
+            expect(article).toHaveProperty('author')
+            expect(article).toHaveProperty('title')
+            expect(article).toHaveProperty('article_id')
+            expect(article).toHaveProperty('body')
+            expect(article).toHaveProperty('topic')
+            expect(article).toHaveProperty('created_at')
+            expect(article).toHaveProperty('votes')
+            expect(article).toHaveProperty('article_img_url')
+        })
     });
-})
+    it('GET:400 sends an appropriate status and error message when given an valid but non-existing id', () => {
+        return request(app)
+          .get('/api/articles/115')
+          .expect(400)
+          .then((response) => {
+            expect(response.body.msg).toBe('Bad Request');
+          });
+      })
+      it('GET:500 sends an appropriate status and error message when given an invalid id', () => {
+        return request(app)
+          .get('/api/articles/non-existing-id')
+          .expect(500)
+          .then((response) => {
+            expect(response.body.msg).toBe('Internal Server Error');
+          });
+      });
+});
